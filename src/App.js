f4095d7ad111e683b88ef009ebd02feade5cd3b2
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-04-02 15:56:39 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Feauture from './components/Feauture';
 import Guide from './components/Guide';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <Route path='/feature' component ={Feauture}/>
         <Route path='/guide' component ={Guide}/> 
       </Switch>
-     
+      <Footer copy="2021" developer="Developed by Samwel Mchomvu"/>
     </div>
   );
 }
